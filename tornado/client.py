a785480a84e28eb4ad78f855cdc155c8773c866b
longfu <haoshuai9291@163.com>
2023-05-11 17:40:57 +0800

    更新测试数据

--- tornado/client.py
+++ tornado/client.py
@@ -15,12 +15,10 @@ def get_encoding_from_reponse(reponse):
     else:
         return requests.utils.get_encoding_from_headers(reponse.headers)
 
-url = "http://0.0.0.0:9888/api/v1/model/hello"
+url = "http://0.0.0.0:9888/api/v1/model/spec"
 
-dic = {
-    
-}
-
+# dic = {"kind":"Model","apiVersion":"inference.haoshuai32.github.com/v1","metadata":{"name":"model-sample","namespace":"default","uid":"c4d4c8a0-9123-49fc-b14b-922e498b7124","resourceVersion":"251628","generation":1,"creationTimestamp":"2023-05-11T02:59:15Z","labels":{"app.kubernetes.io/created-by":"inference","app.kubernetes.io/instance":"model-sample","app.kubernetes.io/managed-by":"kustomize","app.kubernetes.io/name":"model","app.kubernetes.io/part-of":"inference"},"managedFields":[{"manager":"kubectl-create","operation":"Update","apiVersion":"inference.haoshuai32.github.com/v1","time":"2023-05-11T02:59:15Z","fieldsType":"FieldsV1","fieldsV1":{"f:metadata":{"f:labels":{".":{},"f:app.kubernetes.io/created-by":{},"f:app.kubernetes.io/instance":{},"f:app.kubernetes.io/managed-by":{},"f:app.kubernetes.io/name":{},"f:app.kubernetes.io/part-of":{}}},"f:spec":{".":{},"f:data":{".":{},"f:name":{},"f:path":{},"f:retrymax":{},"f:sha":{},"f:url":{}},"f:nodeSelector":{".":{},"f:kubernetes.io/os":{}}}}}]},"spec":{"nodeSelector":{"kubernetes.io/os":"linux"},"data":{"retrymax":3,"url":"htttps://onelcat.github.com","name":"frace","path":"/data","sha":"b5ee974c3d0332a669a638fc7b64a155fe1ec0cfad03bde18a643a6724e188a2"}},"status":{}}
+dic = {"kind":"Model","apiVersion":"inference.haoshuai32.github.com/v1","metadata":{"name":"model-sample-1","namespace":"default","uid":"983ed272-3de8-49c5-ba21-53d347d282fd","resourceVersion":"253613","generation":1,"creationTimestamp":"2023-05-11T03:22:24Z","labels":{"app.kubernetes.io/created-by":"inference","app.kubernetes.io/instance":"model-sample","app.kubernetes.io/managed-by":"kustomize","app.kubernetes.io/name":"model","app.kubernetes.io/part-of":"inference"},"managedFields":[{"manager":"kubectl-create","operation":"Update","apiVersion":"inference.haoshuai32.github.com/v1","time":"2023-05-11T03:22:24Z","fieldsType":"FieldsV1","fieldsV1":{"f:metadata":{"f:labels":{".":{},"f:app.kubernetes.io/created-by":{},"f:app.kubernetes.io/instance":{},"f:app.kubernetes.io/managed-by":{},"f:app.kubernetes.io/name":{},"f:app.kubernetes.io/part-of":{}}},"f:spec":{".":{},"f:data":{".":{},"f:name":{},"f:path":{},"f:retrymax":{},"f:sha":{},"f:url":{}},"f:nodeSelector":{".":{},"f:kubernetes.io/os":{}}}}}]},"spec":{"nodeSelector":{"kubernetes.io/os":"linux"},"data":{"retrymax":3,"url":"htttps://onelcat.github.com","name":"frace-1","path":"/data","sha":"b5ee974c3d0332a669a638fc7b64a155fe1ec0cfad03bde18a643a6724e188a2"}},"status":{}}
 r = requests.post(url,json=dic)
 
 print(r.status_code)
