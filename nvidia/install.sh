f93efc79ea02712271e0a9ec00a5e2e92bc162a6
longfu <haoshuai9291@163.com>
2023-11-17 10:39:09 +0800

    增加nv 显卡驱动安装 cuda安装 卸载

---
+++ nvidia/install.sh
@@ -0,0 +1,13 @@
+sudo apt-get install -y cuda-drivers
+
+sudo apt-get install -y nvidia-kernel-open-545
+
+sudo apt-get install -y cuda-drivers-545
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.3.1/local_installers/cuda-repo-ubuntu2004-12-3-local_12.3.1-545.23.08-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-12-3-local_12.3.1-545.23.08-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2004-12-3-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-12-3
---
+++ nvidia/uninstall.sh
@@ -0,0 +1,6 @@
+sudo apt-get --purge remove "*cuda*" "*cublas*" "*cufft*" "*cufile*" "*curand*" \
+ "*cusolver*" "*cusparse*" "*gds-tools*" "*npp*" "*nvjpeg*" "nsight*" "*nvvm*"
+
+sudo apt-get --purge remove "*nvidia*" "libxnvctrl*"
+
+sudo apt-get autoremove
